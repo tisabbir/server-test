b72ffa720a15ffdc63accc7f398b09cfcd5afcc0
TI Sabbir <tisabbir0@gmail.com>
2024-04-21 15:04:50 +0600

    phones json file added, nodemon problem solved

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=>{
-    res.send('Hello World 292')
+    res.send('Hello World from nodemon')
 });
 
 app.get('/data', (req, res)=>{
