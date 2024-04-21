6f9358f811ae0114c9a766b0cc60c0ec8045a1a1
TI Sabbir <tisabbir0@gmail.com>
2024-04-21 16:11:08 +0600

    server worked

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 const express = require("express");
+const phones = require('./phones.json');
+const cors = require('cors');
+
+
+
 const app = express();
 const port = 3000;
 
+
+app.use(cors())
 app.get('/', (req, res)=>{
     res.send('Hello World from nodemon')
 });
@@ -9,6 +16,24 @@ app.get('/', (req, res)=>{
 app.get('/data', (req, res)=>{
     res.send('All the data you want is here bro......')
 })
+
+app.get('/phones', (req, res)=>{
+    res.send(phones)
+})
+
+app.get('/phones/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log('requested for id :', id);
+    // const index = id - 1;
+    // res.send(phones[index])
+
+    const phone = phones.find(phone => phone.id === id);
+    res.send(phone)
+
+
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`Hello world is listening from ${port}`);
